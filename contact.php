4473eeba30a0703731879558399dd0d445766c81
TristanG6D <trda62504@eleve.isep.fr>
2024-11-25 14:26:06 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil de l'utilisateur</title>
-    <link rel="stylesheet" href="style/styleprofil.css">
+    <link rel="stylesheet" href="styleprofil.css">
 </head>
 <body>
 
