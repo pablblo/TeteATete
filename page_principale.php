2837ec812058ed738a1c6bae97b8dd4fece0932e
Minasdao <minasdaoud27@gmail.com>
2024-11-29 13:49:47 +0100

    Update page_principale.php

--- page_principale.php
+++ page_principale.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create_course'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tête à Tête - Accueil</title>
-    <link rel="stylesheet" href="style/styleprofil.css">
+    <link rel="stylesheet" href="styleprofil.css">
 </head>
 <body>
     
