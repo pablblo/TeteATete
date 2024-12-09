1dfde1171c0f807fee4193b2cc33608651b567ff
Minasdao <minasdaoud27@gmail.com>
2024-12-09 15:01:05 +0100

    Add files via upload

--- BDD_TAT.sql
+++ BDD_TAT.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 09 déc. 2024 à 14:57
+-- Généré le : lun. 09 déc. 2024 à 15:00
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -20,6 +20,51 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `bdd_tat`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+CREATE TABLE `cours` (
+  `idCours` int(11) NOT NULL,
+  `Titre` varchar(45) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `Heure` time DEFAULT NULL,
+  `Taille` int(11) DEFAULT NULL,
+  `Places_restants_Tuteur` int(11) DEFAULT NULL,
+  `Places_restants_Eleve` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evaluation`
+--
+
+CREATE TABLE `evaluation` (
+  `idEvaluation` int(11) NOT NULL,
+  `Tuteur_ou_Eleve` tinyint(4) DEFAULT NULL,
+  `Note` tinyint(4) DEFAULT NULL,
+  `Commentaire` varchar(200) DEFAULT NULL,
+  `idUserAuteur` int(11) NOT NULL,
+  `idUserReceveur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message_contact`
+--
+
+CREATE TABLE `message_contact` (
+  `idMessage_Contact` int(11) NOT NULL,
+  `Mail` varchar(100) DEFAULT NULL,
+  `Message` varchar(250) DEFAULT NULL,
+  `idUserAuteur` int(11) NOT NULL,
+  `idUserReceveur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -41,9 +86,44 @@ CREATE TABLE `user` (
   `reset_expiration` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_cours`
+--
+
+CREATE TABLE `user_cours` (
+  `idUser_Cours` int(11) NOT NULL,
+  `Tuteur_ou_Eleve` tinyint(4) DEFAULT NULL,
+  `idUser` int(11) NOT NULL,
+  `idCours` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `cours`
+--
+ALTER TABLE `cours`
+  ADD PRIMARY KEY (`idCours`);
+
+--
+-- Index pour la table `evaluation`
+--
+ALTER TABLE `evaluation`
+  ADD PRIMARY KEY (`idEvaluation`),
+  ADD KEY `fk_Evaluation_User1_idx` (`idUserReceveur`),
+  ADD KEY `fk_UserAuteur` (`idUserAuteur`);
+
+--
+-- Index pour la table `message_contact`
+--
+ALTER TABLE `message_contact`
+  ADD PRIMARY KEY (`idMessage_Contact`),
+  ADD KEY `fk_idUserAuteur_idx` (`idUserAuteur`),
+  ADD KEY `fk_idUserReceveur_idx` (`idUserReceveur`);
 
 --
 -- Index pour la table `user`
@@ -53,14 +133,71 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `Mail` (`Mail`);
 
 --
+-- Index pour la table `user_cours`
+--
+ALTER TABLE `user_cours`
+  ADD PRIMARY KEY (`idUser_Cours`),
+  ADD KEY `fk_idUser_idx` (`idUser`),
+  ADD KEY `fk_idCours_idx` (`idCours`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `cours`
+--
+ALTER TABLE `cours`
+  MODIFY `idCours` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `evaluation`
+--
+ALTER TABLE `evaluation`
+  MODIFY `idEvaluation` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `message_contact`
+--
+ALTER TABLE `message_contact`
+  MODIFY `idMessage_Contact` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
   MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `user_cours`
+--
+ALTER TABLE `user_cours`
+  MODIFY `idUser_Cours` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `evaluation`
+--
+ALTER TABLE `evaluation`
+  ADD CONSTRAINT `fk_UserAuteur` FOREIGN KEY (`idUserAuteur`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_UserReceveur` FOREIGN KEY (`idUserReceveur`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `message_contact`
+--
+ALTER TABLE `message_contact`
+  ADD CONSTRAINT `fk_idUserAuteur` FOREIGN KEY (`idUserAuteur`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idUserReceveur` FOREIGN KEY (`idUserReceveur`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `user_cours`
+--
+ALTER TABLE `user_cours`
+  ADD CONSTRAINT `fk_idCours` FOREIGN KEY (`idCours`) REFERENCES `cours` (`idCours`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idUser` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
