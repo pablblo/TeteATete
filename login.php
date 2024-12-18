e1edc0b4a3cf4fc65f6358d3b10c21015d656dde
Alexandre Sassi <71190353+sassial@users.noreply.github.com>
2024-12-18 19:10:44 +0100

    update favicon

--- login.php
+++ login.php
@@ -44,6 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <link rel="icon" type="image/x-icon" href="images/logo.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tête à Tête - Connexion</title>
